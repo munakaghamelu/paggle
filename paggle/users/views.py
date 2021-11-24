a008f97d6e4477420b415aa53173ef6d7da3a077
munakaghamelu <mgodwinaghamelu@yahoo.co.uk>
2021-11-24 17:54:35 +0000

    need to complete authentication

--- paggle/users/views.py
+++ paggle/users/views.py
@@ -5,11 +5,14 @@ from django.contrib import messages
 # Create your views here.
 def register(request):
     if request.method == 'POST':
+
         # If we get a post request create a form with the requested data, else it's a GET so we just display a blank form
         form = UserCreationForm(request.POST)
+
         # need to check if authentication code is valid
         if form.is_valid():
             username = form.cleaned_data.get('username')
+            
             # flash message to show we've received the data
             messages.success(request, f'Account created for {username}!')
             return redirect('paggle-selectTask')
