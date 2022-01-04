9667730f59f0cf8faccf9c253cd1258d00bbc0a0
munakaghamelu <mgodwinaghamelu@yahoo.co.uk>
2022-01-04 15:35:19 +0000

    added remaining csvs, need to upload data to django

--- preprocessing.py
+++ preprocessing.py
@@ -9,13 +9,13 @@ from pathlib import Path
 df = pd.read_excel('./s3_bucket_data.xlsx')
 
 with open('./paggle/ham10000_images.csv', 'w') as f:
-    f.write('image_id,link,type\n')
+    f.write('dataset_id,image_id,link,type\n')
     # read line in df and write
     for row in range(len(df)):
         image_name = df.iloc[row,0]
         image_id = image_name.partition(".")[0]
         type = image_name.partition(".")[2]
         link = df.iloc[row,1]
-        f.write(f'{image_id},{link},{type}\n')
+        f.write(f'1,{image_id},{link},{type}\n')
 
 print("complete.")
