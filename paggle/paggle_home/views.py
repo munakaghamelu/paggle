8a3df9437ff5a6aa86dbcc6d2eb0551ea05d9f4f
munakaghamelu <mgodwinaghamelu@yahoo.co.uk>
2022-01-05 17:53:10 +0000

    completed query to retrieve datasets

--- paggle/paggle_home/views.py
+++ paggle/paggle_home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from .models import Dataset
 
 def home(request):
     return render(request, 'paggle_home/home.html')
@@ -12,6 +14,11 @@ def selectTask(request):
 @login_required
 def selectData(request):
     return render(request, 'paggle_home/selectData.html')
+
+class DatasetListView(ListView):
+    model = Dataset
+    template_name= 'paggle_home/selectData.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'datasets'
 
 @login_required
 def selectModel(request):
